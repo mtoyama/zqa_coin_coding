fbfa91fb8518fdee6c8e757065fbf58aab42bfbc
kristina-sqa <kristinasqa@gmail.com>
2021-05-12 17:35:25 +0000

    adding list file

--- users/kristina/coin_api/coin_api_test.py
+++ users/kristina/coin_api/coin_api_test.py
@@ -1,29 +1,41 @@
 #from api_patches.coinapi_patch import CoinAPIv1Patched
 from coinapi_rest_v1.restapi import CoinAPIv1
 import datetime, os
+import pandas as pd
 
 test_key = os.environ.get("COIN_API_KEY")
 #api = CoinAPIv1(os.environ['COIN_API_KEY'])
 
 api = CoinAPIv1(test_key)
-exchanges = api.metadata_list_exchanges()
+exchanges_btc = api.exchange_rates_get_all_current_rates("BTC")
+# print(type(exchanges_btc))
+df_btc = pd.DataFrame(exchanges_btc)
 
-print('Exchanges')
-for exchange in exchanges:
-    print('Exchange ID: %s' % exchange['exchange_id'])
-    print('Exchange website: %s' % exchange['website'])
-    print('Exchange name: %s' % exchange['name'])
+# print(df_btc.info())
+# print((exchanges_btc['asset_id_base']))
+# print(exchanges_btc['rates'])
+df_test = pd.DataFrame(exchanges_btc['rates'])
+df_test.to_csv('list_rates.csv',index=False)
+# print (df_test.describe())
+# exchanges = api.metadata_list_exchanges()
 
 
-exchange_rate = api.exchange_rates_get_specific_rate('BTC', 'USD')
-print('Time: %s' % exchange_rate['time'])
-print('Base: %s' % exchange_rate['asset_id_base'])
-print('Quote: %s' % exchange_rate['asset_id_quote'])
-print('Rate: %s' % exchange_rate['rate'])
-last_week = datetime.date(2017, 5, 16).isoformat()
+# print('Exchanges')
+# for exchange in exchanges:
+#     print('Exchange ID: %s' % exchange['exchange_id'])
+#     print('Exchange website: %s' % exchange['website'])
+#     print('Exchange name: %s' % exchange['name'])
 
-exchange_rate_last_week = api.exchange_rates_get_specific_rate('BTC', 'USD', {'time': last_week})
-print('Time: %s' % exchange_rate_last_week['time'])
-print('Base: %s' % exchange_rate_last_week['asset_id_base'])
-print('Quote: %s' % exchange_rate_last_week['asset_id_quote'])
-print('Rate: %s' % exchange_rate_last_week['rate'])
+
+# exchange_rate = api.exchange_rates_get_specific_rate('BTC', 'USD')
+# print('Time: %s' % exchange_rate['time'])
+# print('Base: %s' % exchange_rate['asset_id_base'])
+# print('Quote: %s' % exchange_rate['asset_id_quote'])
+# print('Rate: %s' % exchange_rate['rate'])
+# last_week = datetime.date(2017, 5, 16).isoformat()
+
+# exchange_rate_last_week = api.exchange_rates_get_specific_rate('BTC', 'USD', {'time': last_week})
+# print('Time: %s' % exchange_rate_last_week['time'])
+# print('Base: %s' % exchange_rate_last_week['asset_id_base'])
+# print('Quote: %s' % exchange_rate_last_week['asset_id_quote'])
+# print('Rate: %s' % exchange_rate_last_week['rate'])
