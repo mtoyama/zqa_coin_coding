5fd20c9492a23efb1afa2b04115538353ef23bff
kristina-sqa <kristinasqa@gmail.com>
2021-05-17 17:59:23 +0000

    filter data

--- users/kristina/coin_api/coin_api_test.py
+++ users/kristina/coin_api/coin_api_test.py
@@ -2,6 +2,7 @@
 from coinapi_rest_v1.restapi import CoinAPIv1
 import datetime, os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 test_key = os.environ.get("COIN_API_KEY")
 #api = CoinAPIv1(os.environ['COIN_API_KEY'])
@@ -16,7 +17,22 @@ df_btc = pd.DataFrame(exchanges_btc)
 # print(exchanges_btc['rates'])
 df_test = pd.DataFrame(exchanges_btc['rates'])
 df_test.to_csv('list_rates.csv',index=False)
-# print (df_test.describe())
+df_test['asset_id_quote'] == 'USD'
+
+AB = df_test['asset_id_quote'] == 'USD'
+BC = df_test['asset_id_quote'] == 'DOGE'
+CD = df_test['asset_id_quote'] == 'GOLD'
+DE = df_test['asset_id_quote'] == 'ETH'
+EF = df_test['asset_id_quote'] == 'XRP'
+print (df_test[AB|BC|CD|DE|EF])
+
+plt.bar(df_test['asset_id_quote'], df_test['rate'])
+plt.show()
+
+
+# print (type)
+# print (type(df_test['asset_id_quote']))
+# print (df_test)
 # exchanges = api.metadata_list_exchanges()
 
 
